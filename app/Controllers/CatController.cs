040b4afb8b88d5dfa99ec6634b583001463b44e2
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2019-06-03 16:01:18 +0100

    Test of 1.0 controllers in 2.1 project

---
+++ app/Controllers/CatController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowWorld.Controllers
+{
+    public class CatController : Controller
+    {
+        [HttpGet("cat/{cat}")]
+        public string Cat(string cat)
+        {
+            switch (cat)
+            {
+                case "winnie":
+                    return "Meow! Hiss! Meow!";
+                case "molly":
+                    return "Silent Purr. Silent Kill.";
+                case "dexter":
+                    return "Meow. Barf. Meow.";
+                case "kali":
+                    return "Meow. Meow. Meow. Hiss. Bite. Meow.";
+                case "jessie":
+                    return "Pounce. Purr. Pounce. Purr.";
+                case "murphy":
+                    return "Nom nom nom nom nom nom nom nom.";
+            }
+
+            return "No Such cat!";
+        }
+
+        [HttpGet("cat/all")]
+        public IActionResult All()
+        {
+            return View();
+        }
+
+        [HttpGet("cat/version")]
+        public string Version()
+        {
+            return "Version 1.0";
+        }
+
+    }
+}
---
+++ app/Controllers/HordeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowWorld.Controllers
+{
+    public class HordeController : Controller
+    {
+        private static Dictionary<string, string> _Cats = new Dictionary<string, string>();
+
+        [HttpPost("horde/{cat}")]
+        public string Post(string cat, [FromBody]string sound)
+        {
+            _Cats[cat] = sound;
+
+            return String.Format("{0} added to the horde!", cat);
+        }
+
+        [HttpGet("horde/{cat}")]
+        public string Get(string cat)
+        {
+            if (!_Cats.ContainsKey(cat))
+            {
+                return "Cat not found.";
+            }
+
+            return _Cats[cat];
+        }
+
+        [HttpPatch("horde/{cat}")]
+        public string Patch(string cat, [FromBody]string sound)
+        {
+            if (!_Cats.ContainsKey(cat))
+            {
+                return "Cat not found.";
+            }
+
+            _Cats[cat] = sound;
+
+            return "Cat updated.";
+        }
+
+        [HttpDelete("horde/{cat}")]
+        public string Delete(string cat)
+        {
+            if (!_Cats.ContainsKey(cat))
+            {
+                return "Cat not found.";
+            }
+
+            _Cats.Remove(cat);
+
+            return "Cat deleted.";
+        }
+
+        [HttpGet("horde/all")]
+        public IActionResult All()
+        {
+            var cats = new Models.Cats();
+
+            cats.Names = _Cats.Keys.ToArray();
+
+            return View(cats);
+        }
+
+        [HttpGet("horde/cat/{cat}")]
+        public IActionResult Cat(string cat)
+        {
+            Models.Cat model = null;
+
+            if (_Cats.ContainsKey(cat))
+            {
+                model = new Models.Cat
+                {
+                    Name = cat,
+                    Sound = _Cats[cat],
+                };
+            }
+
+            return View(model);
+        }
+
+        [HttpGet("horde/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("horde/add")]
+        public IActionResult Add(string name, string sound)
+        {
+            _Cats[name] = sound;
+
+            return Redirect("/horde/all");
+        }
+    }
+}
